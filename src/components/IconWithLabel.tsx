6dd34602ee1964b46f32057f9d8721aea7642c38
John Sprague <spraguejdev@gmail.com>
2025-05-21 21:25:38 -0500

    feat: create a reusable icons with label for svg icons

---
+++ src/components/IconWithLabel.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface IconProps {
+  children: any;
+  name: string; 
+}
+
+const IconWithLabel: React.FC<IconProps> = ({ children, name }) => {
+  return (
+    <div className="text-center">
+      <div className="p-4 bg-secondary rounded-lg shadow-lg flex flex-col justify-center items-center">
+        <span className="text-4xl">
+          {children}
+        </span>
+        <p className="text-xs mt-2">{name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default IconWithLabel;
