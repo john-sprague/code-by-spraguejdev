7c9b8c0ddd581ab28dfd3d8f1e395b1b0dd705dd
John Sprague <spraguejdev@gmail.com>
2025-05-26 18:57:00 -0500

    refactor: navigation nav items
    - Keep code dry by mapping over nav items
    -  Adjust styles to match
    - Use the nav items interface in foot navigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ScrollToTopButton from "./ScrollToTopButton";
+import { NavItems } from "./MainNavigation";
 
 const Footer: React.FC = () => {
   return (
@@ -9,50 +10,32 @@ const Footer: React.FC = () => {
 
       <div className="px-6 mx-auto max-w-7xl">
         <div className="flex flex-col sm:flex-row justify-between items-center space-y-6 sm:space-y-0">
-          {/* Logo or Brand Name */}
           <div className="text-2xl font-bold text-center sm:text-left">
             <Link to="/" className="hover:text-purple-600">
               John Sprague
             </Link>
           </div>
 
-          {/* Navigation Links */}
+
           <div className="flex space-x-8 justify-center sm:justify-start">
-            <Link
-              to="/skills"
+            {NavItems.map(navItem => (
+              <Link
+              to={navItem.pagePath}
               className="text-lg text-zinc-200 hover:text-zinc-50 transition duration-300"
             >
-              Skills
+              {navItem.pageName}
             </Link>
-            <Link
-              to="/about-me"
-              className="text-lg text-zinc-200 hover:text-zinc-50 transition duration-300"
-            >
-              About Me
-            </Link>
-            <Link
-              to="/projects"
-              className="text-lg text-zinc-200 hover:text-zinc-50 transition duration-300"
-            >
-              Projects
-            </Link>
-            <Link
-              to="/contact"
-              className="text-lg text-zinc-200 hover:text-zinc-50 transition duration-300"
-            >
-              Contact
-            </Link>
+            ))}
           </div>
         </div>
 
         <div className="mt-8 border-t border-zinc-300/50 pt-6 text-center">
-          {/* Social Media Links */}
           <div className="flex justify-center space-x-6 mb-4">
             <a
               href="https://linkedin.com/in/spraguejdev"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-zinc-400 hover:text-zinc-50"
+              className="text-zinc-300 hover:text-zinc-50"
             >
               LinkedIn
             </a>
@@ -60,7 +43,7 @@ const Footer: React.FC = () => {
               href="https://github.com/spraguejdev"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-zinc-400 hover:text-zinc-50"
+              className="text-zinc-300 hover:text-zinc-50"
             >
               GitHub #1 (Older)
             </a>
@@ -68,7 +51,7 @@ const Footer: React.FC = () => {
               href="https://github.com/john-sprague"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-zinc-400 hover:text-zinc-50"
+              className="text-zinc-300 hover:text-zinc-50"
             >
               GitHub #2 (Newer)
             </a>
@@ -76,13 +59,11 @@ const Footer: React.FC = () => {
               href="https://twitter.com/johnsprague"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-zinc-400 hover:text-zinc-50"
+              className="text-zinc-300 hover:text-zinc-50"
             >
               Twitter
             </a>
           </div>
-
-          {/* Copyright */}
           <p className="text-sm text-zinc-400">
             &copy; {new Date().getFullYear()} John Sprague. All rights reserved.
           </p>
--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,50 +1,32 @@
 import { Link } from "react-router-dom";
+interface NavItem {
+  pageName: string;
+  pagePath: string;
+}
+
+export const NavItems: NavItem[] = [
+  { pageName: "Home", pagePath: "/" },
+  { pageName: "Skills", pagePath: "/skills" },
+  { pageName: "About Me", pagePath: "/about-me" },
+  { pageName: "Projects", pagePath: "/projects" },
+  { pageName: "Contact Me", pagePath: "/contact-me" },
+];
 
 const MainNavigation = () => {
   return (
     <header>
       <nav className="my-16 animate-bounce">
         <ul className="flex items-center justify-center gap-5">
-          <li>
-            <Link
-              to="/"
-              className="text-md duration-500 text-zinc-200 hover:text-zinc-50"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/skills"
-              className="text-md duration-500 text-zinc-200 hover:text-zinc-50"
-            >
-              Skills
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about-me"
-              className="text-md duration-500 text-zinc-200 hover:text-zinc-50"
-            >
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/projects"
-              className="text-md duration-500 text-zinc-200 hover:text-zinc-50"
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className="text-md duration-500 text-zinc-200 hover:text-zinc-50"
-            >
-              Contact
-            </Link>
-          </li>
+          {NavItems.map((navItem) => (
+            <li>
+              <Link
+                to={navItem.pagePath}
+                className="text-md duration-500 text-zinc-200 hover:text-zinc-50 text-lg"
+              >
+                {navItem.pageName}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
