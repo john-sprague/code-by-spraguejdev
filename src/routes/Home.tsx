add2375e75a109ebcda0791684859d3e40989cd7
John Sprague <spraguejdev@gmail.com>
2025-05-25 20:04:58 -0500

    refactor(homepage): update homepage styles
    - remove in page navigation
    - add call to action buttons
    - update section styles
    - adjust styling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import JavaScriptIcon from "../assets/icons/javascript.svg";
 import TypeScriptIcon from "../assets/icons/typescript.svg";
 import ReactIcon from "../assets/icons/react.svg";
@@ -7,59 +7,39 @@ import SqlDeveloperIcon from "../assets/icons/sqldeveloper.svg";
 import PythonIcon from "../assets/icons/python.svg";
 import CssIcon from "../assets/icons/css.svg";
 import HTML5Icon from "../assets/icons/html5.svg";
+import Button from "../components/Button";
 
 const Home = () => {
-  const navigation = [
-    { to: "skills", name: "Skills" },
-    { to: "about-me", name: "About Me" },
-    { to: "projects", name: "Projects" },
-    { to: "contact", name: "Contact" },
-  ];
+  const navigate = useNavigate();
+
+  const navigateToProjects = () => {
+    navigate("/projects");
+  };
+
+  const navigateToContact = () => {
+    navigate("/contact");
+  };
 
   return (
     <div className="relative pb-16 min-h-screen">
-      {/* Navigation */}
-      <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-        <nav className="my-16 animate-bounce">
-          <ul className="flex items-center justify-center gap-5">
-            {navigation.map((item) => (
-              <Link
-                key={item.to}
-                className="text-md text-zinc-200 hover:text-zinc-50 transition duration-500"
-                to={item.to}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </ul>
-        </nav>
-
+      <div className="px-6 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16">
         <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-
-        {/* Section Title */}
         <div className="text-center">
-          <h1 className=" pb-10 text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white via-orange-400 to-blue-600 animate-title sm:text-6xl md:text-9xl">
+          <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white via-orange-400 to-blue-600 animate-title sm:text-6xl md:text-9xl">
             John Sprague
           </h1>
+          <h2 className="text-2xl text-zinc-300 pt-8 animate-fade-in">Full-Stack Engineer and Builder of Useful Things</h2>
         </div>
 
         <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
-        {/* Section Content */}
-        <div className="my-16 text-center animate-fade-in">
-          <h2 className="text-md text-zinc-500">
-            As a Senior Software Engineer with a proven track record at IBM, I
-            specialize in full-stack development, UX design, and optimizing
-            system performance to deliver high-impact, scalable solutions.
+        <div className="mr-8 ml-8 p-8 rounded-2xl shadow-lg border border-zinc-300 animate-fade-in">
+          <p className="text-lg text-zinc-300 leading-relaxed animate-fade-in" style={{ marginTop: "0px"}} >
+            As a Senior Software Engineer with a proven track record at IBM, I specialize in full-stack development, UX design, and optimizing system performance to deliver high-impact, scalable solutions.
             <br />
             <br />
-            Recognized with an Outstanding Technical Achievement Award (OTAA)
-            and promoted three times in just three years, I excel at
-            transforming complex challenges into user-centric, business-driving
-            applications.
-          </h2>
-
-          {/* Skill Icons */}
+            Recognized with an Outstanding Technical Achievement Award (OTAA) and promoted three times in just three years, I excel at transforming complex challenges into user-centric, business-driving applications.
+          </p>
           <div className="flex items-center justify-center gap-2 mt-5">
             <JavaScriptIcon />
             <TypeScriptIcon />
@@ -70,6 +50,22 @@ const Home = () => {
             <CssIcon />
             <HTML5Icon />
           </div>
+        </div>
+        <div className="mr-8 ml-8 flex justify-center gap-6 animate-fade-in">
+          <Button
+            variant="default"
+            className="px-8 py-3 text-lg font-semibold text-white"
+            onClick={navigateToProjects} // Adding onClick event
+          >
+            View Projects
+          </Button>
+          <Button
+            variant="secondary"
+            className="px-8 py-3 text-lg font-semibold text-white"
+            onClick={navigateToContact}
+          >
+            Contact Me
+          </Button>
         </div>
       </div>
     </div>
