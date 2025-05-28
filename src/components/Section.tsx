c71a567faad54ecd1bdfe2720859137cfd3545a0
John Sprague <spraguejdev@gmail.com>
2025-05-28 13:45:42 -0500

    refactor(section): add support for a section image

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -7,7 +7,9 @@ interface SectionProps {
   children?: React.ReactNode;
   isDarkMode?: boolean;
   isSingleColumn?: boolean;
-  alignTitle?: string;
+  alignTitle?: "left" | "right";
+  imageSrc?: string;
+  imageAlt?: string;
 }
 
 const Section: React.FC<SectionProps> = ({
@@ -18,35 +20,42 @@ const Section: React.FC<SectionProps> = ({
   isDarkMode = false,
   isSingleColumn = false,
   alignTitle = "left",
+  imageSrc,
+  imageAlt = "Section Image",
 }) => {
   return (
     <>
       <section
         className={`${
-          isDarkMode ? `` : `bg-zinc-900`
+          isDarkMode ? "" : "bg-zinc-900"
         } min-h-[60vh] w-full text-zinc-100 flex items-center py-12 md:px-8 xl:px-20 animate-fade-in`}
       >
         <div
-          className={`container flex flex-col ${
+          className={`container w-full gap-16 ${
             isSingleColumn
-              ? "items-center"
-              : `md:flex-row ${isDarkMode ? "items-start" : "md:flex-row-reverse items-end"}`
-          } gap-16 w-full`}
+              ? "flex flex-col items-center"
+              : `flex flex-col md:flex-row ${
+                  isDarkMode
+                    ? "items-stretch"
+                    : "md:flex-row-reverse items-stretch"
+                }`
+          }`}
         >
+
           <div
             className={`relative p-6 sm:px-28 md:p-0 w-full ${
               isSingleColumn ? "" : "md:w-1/2"
-            }`}
+            } flex flex-col justify-center`}
           >
-            {/* Vertical Text */}
+
             <div
-              className={`absolute -left-4 sm:left-16 sm:top-24 md:-left-12 xl:-left-16 top-24 rotate-[-90deg] text-sm tracking-widest flex flex-row justify-start gap-2`}
+              className="absolute -left-4 sm:left-16 sm:top-24 md:-left-12 xl:-left-16 top-24 rotate-[-90deg] text-sm tracking-widest flex flex-row justify-start gap-2"
             >
               <div className="xl:w-16 md:w-10 sm:w-8 w-8 h-[2px] bg-white mt-2 mx-auto"></div>
               <p>{verticalLineText}</p>
             </div>
 
-            {/* Heading and Subtitle */}
+
             <div
               className={`text-zinc-100 p-6 pr-5 pl-12 lg:pr-20 ${
                 alignTitle === "right" ? "text-right" : "text-left"
@@ -58,11 +67,29 @@ const Section: React.FC<SectionProps> = ({
               <p className="mt-4 text-lg">{subtitle}</p>
             </div>
 
-            {/* Children */}
-            {children && children}
+
+            {children}
           </div>
+
+
+          {imageSrc && (
+            <div
+              className={`w-full ${
+                isSingleColumn ? "" : "md:w-1/2"
+              } flex justify-center items-center`}
+            >
+              <div className="w-full h-full flex justify-center items-center">
+                <img
+                  src={imageSrc}
+                  alt={imageAlt}
+                  className="max-w-full max-h-[400px] object-cover rounded-2xl shadow-lg transition-transform duration-300 transform hover:scale-125"
+                />
+              </div>
+            </div>
+          )}
         </div>
       </section>
+
       <div className="w-full h-px bg-zinc-600 my-6 animate-fade-in" />
     </>
   );
